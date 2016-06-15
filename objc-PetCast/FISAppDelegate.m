1d328bef6463667a651186aab9428c6eeed3744b
SusanZheng15 <zheng.susan12@gmail.com>
2016-06-15 16:54:03 -0400

    Done

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,10 +1,25 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISPet.h"
+#import "FISDog.h"
 
 @implementation FISAppDelegate
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    FISDog *dog = [[FISDog alloc]init];
+    
+    [dog makeASound];
+    [dog assaultTheMailman];
+    
+    NSLog(@"%@", [dog makeASound]);
+    NSLog(@"%@", [dog assaultTheMailman]);
+    
+    FISPet *anotherDog = [[FISDog alloc]init];
+    [(FISDog *)anotherDog assaultTheMailman];
+    
+    NSLog(@"%@", anotherDog);
+    
     return YES;
 }
 
